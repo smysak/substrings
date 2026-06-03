8994387ba79ebdd1d16c273199a49dcd17c6ef50
smysak <98991422+smysak@users.noreply.github.com>
2026-06-03 08:39:06 +0800

    create three dictionaries (adj., verbs, nouns)

---
+++ substrings.rb
@@ -0,0 +1,6 @@
+adjectives = [ white, black, red, yellow, blue, open, shut, large, small, cute, furry, friendly, scary ]
+nouns = [ door, monster, sword, chest, treasure, wall, window, room, pit, me, myself, enemy ]
+verbs = [ look, try, attack, open, close, enter, take, get, give, kill, hit, pat, poke, peek ]
+
+def substrings(words, dictionary)
+    
