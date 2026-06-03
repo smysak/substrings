6dbe0f2cd5eb6eed10008596579e6990282b0a0d
smysak <98991422+smysak@users.noreply.github.com>
2026-06-03 16:04:48 +0800

    fix arrays to hold strings not undefined variables add a regex to control dictionary choice

--- substrings.rb
+++ substrings.rb
@@ -1,9 +1,9 @@
-adjectives = [ white, black, red, yellow, blue, open, shut, large, small, cute, furry, friendly, scary ]
-nouns = [ door, monster, sword, chest, treasure, wall, window, room, pit, me, myself, enemy ]
-verbs = [ look, try, attack, open, close, enter, take, get, give, kill, hit, pat, poke, peek ]
+ADJECTIVES = %w[ white black red yellow blue open shut large small cute furry friendly scary ]
+NOUNS = %w[ door monster sword chest treasure wall window room pit me myself enemy ]
+VERBS = %w[ look try attack open close enter take get give kill hit pat poke peek ]
 
-def substrings(words, dictionary)
-
+def substrings(words dictionary)
+    puts "hello. this isn't working yet."
 end
 
 
@@ -18,8 +18,17 @@ def choose_dictionary
     puts ""
     puts "Enter the dictionary:"
     dictionary = gets.strip.downcase
+
+    if dictionary.match?(/^(?:adjectives|nouns|verbs)$/)
+        puts ""
+    else
+        puts "'#{dictionary}' is not a valid dictionary."
+        exit(1)
+    end
+
+    dictionary
 end
 
 
-substrings(get_words, choose_dictionary)
+substrings(get_words choose_dictionary)
 
