5f715f8fd27805f971ba901a48dd625852bb570b
smysak <98991422+smysak@users.noreply.github.com>
2026-06-04 13:22:08 +0800

    fix formatting to ruby standards

--- substrings.rb
+++ substrings.rb
@@ -1,49 +1,51 @@
+# encoding: utf-8
+
 DICTIONARIES = {
-    adjectives: %w[ white black red yellow blue open shut large small cute furry friendly scary ],
-    nouns: %w[ door monster sword chest treasure wall window room pit me myself enemy ],
-    verbs: %w[ look try attack open close enter take get give kill hit pat poke peek ]
+  adjectives: %w[ white black red yellow blue open shut large small cute furry friendly scary ],
+  nouns: %w[ door monster sword chest treasure wall window room pit me myself enemy ],
+  verbs: %w[ look try attack open close enter take get give kill hit pat poke peek ],
 }
 
 def substrings(string, dictionary)
-    puts "\nYou have chosen to search in '#{dictionary}'."
-    result = Hash.new(0)
-    DICTIONARIES[dictionary].each do |word|
-        matches = string.scan(word).length
-        if matches > 0
-            result[word] = matches
-        end
+  puts "\nYou have chosen to search in '#{dictionary}'."
+  result = Hash.new(0)
+  DICTIONARIES[dictionary].each do |word|
+    matches = string.scan(word).length
+    if matches > 0
+      result[word] = matches
     end
-    puts "\n" + "=" * 37
-    puts "RESULTS"
-    puts "=" * 37
-    if result.empty?
-        puts "\nYou have found no matches in '#{dictionary}'."
-    else
-        puts "\nYou have found the following matches:"
-        result.each do |word, matches|
-            puts "  ‣ #{word.capitalize} X #{matches}"
-        end
+  end
+  puts "\n" + "=" * 37
+  puts "RESULTS"
+  puts "=" * 37
+  if result.empty?
+    puts "\nYou have found no matches in '#{dictionary}'."
+  else
+    puts "\nYou have found the following matches:"
+    result.each do |word, matches|
+      puts "  ‣ #{word.capitalize} X #{matches}"
     end
-    puts "\n" + "=" * 37
+  end
+  puts "\n" + "=" * 37
 end
 
 def get_string
-    puts "\nEnter the string:"
-    string = gets.strip.downcase
+  puts "\nEnter the string:"
+  string = gets.strip.downcase
 end
 
 def choose_dictionary
-    puts "\nEnter the dictionary:"
-    dictionary = gets.strip.downcase
+  puts "\nEnter the dictionary:"
+  dictionary = gets.strip.downcase
 
-    if dictionary.match?(/^(?:adjectives|nouns|verbs)$/)
-        dictionary = dictionary.to_sym
-    else
-        puts "'#{dictionary}' is not a valid dictionary."
-        exit(1)
-    end
+  if dictionary.match?(/^(?:adjectives|nouns|verbs)$/)
+    dictionary = dictionary.to_sym
+  else
+    puts "'#{dictionary}' is not a valid dictionary."
+    exit(1)
+  end
 
-    dictionary
+  dictionary
 end
 
 substrings(get_string, choose_dictionary)
